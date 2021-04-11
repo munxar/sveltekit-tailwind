3fbc7e33e02e0f087e8e203a766c8a25452f14be
Sascha Aeppli <sascha.aeppli@liip.ch>
2021-04-11 08:26:53 +0200

    update readme. dev tools integration

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import 'virtual:windi.css';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		import('virtual:windi-devtools');
+	});
 </script>
 
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,6 @@
-<h1 class="bg-gray-100 p-2 text-center">Hello, Tailwind!</h1>
+<h1 class="bg-gray-100 p-2 text-center shadow">Hello, Tailwind!</h1>
+<p class="p-2">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates esse, non fugiat nisi sapiente
+	laborum sed modi, facilis itaque blanditiis dicta officia excepturi totam vero nesciunt
+	consectetur illo, ex earum.
+</p>
