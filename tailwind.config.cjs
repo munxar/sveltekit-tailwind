81a11a9fff37279789524c834a1d69db193a23bb
Sascha Aeppli <sascha.aeppli@liip.ch>
2021-04-11 09:09:10 +0200

    update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
     dark: 'class',
     plugins: [
-        require('windicss/plugin/forms')
+        require('windicss/plugin/forms'),
+        require('windicss/plugin/aspect-ratio'),
+        require('windicss/plugin/line-clamp'),
+        require('windicss/plugin/filters'),
+        require('windicss/plugin/scroll-snap'),     
     ],
 }
