687a870a8adda518a2feceb12996df28a76a35fa
Sascha Aeppli <sascha.aeppli@liip.ch>
2021-09-15 06:30:03 +0200

    added example for style overwrite.

---
+++ src/lib/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let className = '';
+	export { className as class };
+</script>
+
+<button
+	class={`bg-red-500 dark:bg-purple-200 text-white dark:text-gray-900 hover:opacity-80 px-4 py-2 rounded ${className}`}
+	><slot /></button
+>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,13 @@
+<script>
+	import Button from '$lib/Button.svelte';
+</script>
+
 <p class="p-2 dark:(bg-gray-900 text-white)">
 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates esse, non fugiat nisi sapiente
 	laborum sed modi, facilis itaque blanditiis dicta officia excepturi totam vero nesciunt
 	consectetur illo, ex earum.
 </p>
+<div class="p-2 dark:(bg-gray-900 text-white)">
+	<Button>Default Style</Button>
+	<Button class="bg-blue-400">Overwrite Style</Button>
+</div>
